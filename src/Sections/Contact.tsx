a56ad986c73701074a6edfa407fdd5bdf75324bf
DavidRodriguez <studiandoprogramacion@gmail.com>
2026-03-18 16:53:43 -0600

    feat: new section +++Contact+++ created

---
+++ src/Sections/Contact.tsx
@@ -0,0 +1,11 @@
+const Contact: React.FC = () => {
+    return (
+        <section className="mt-10 pt-10 px-10 flex flex-col items-center justify-center gap-5 bg-[#FEE349] shadow-lg " id="contact">
+            <h2 className="font-poppins font-semibold text-2xl text-center">Get in Touch</h2>
+            <h3 className="font-poppins font-normal text-lg text-center">I'm always open to discussing new projects, creative ideas, or opportunities to be part of your visions. Feel free to reach out!</h3>
+        </section>
+
+    );
+};
+
+export default Contact;
