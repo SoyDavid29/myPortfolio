c5c6c982d94bee63a982bb06cc796a118da967ca
DavidRodriguez <studiandoprogramacion@gmail.com>
2026-03-18 17:36:54 -0600

    feat: footer created

---
+++ src/Sections/Footer.tsx
@@ -0,0 +1,8 @@
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-gray-800 text-white py-4 text-center">
+        <p>&copy; {new Date().getFullYear()} Your Company. All rights reserved.</p>
+    </footer>
+  );
+};
+export default Footer;
