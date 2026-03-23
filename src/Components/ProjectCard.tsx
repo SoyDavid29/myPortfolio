456f33255e441574b5da9cd0bfad05a1c13245f8
DavidRodriguez <studiandoprogramacion@gmail.com>
2026-03-23 12:28:30 -0600

    style: animation on projects already done

--- src/Components/ProjectCard.tsx
+++ src/Components/ProjectCard.tsx
@@ -1,4 +1,6 @@
 import type { Project } from "../db/ProjectsDb";
+import { useRef } from "react";
+import { motion, useInView } from "framer-motion";
 
 import typescript from "../assets/typescriptlogo.png";
 import react from "../assets/reactlogo.png";
@@ -8,18 +10,29 @@ import www from "../assets/wwwlogo.png";
 
 const ProjectCard: React.FC<Project> = ({ img, tittle, description, link }) => {
 
+    const ref = useRef<HTMLDivElement>(null);
+    const isInView = useInView(ref, { amount: 0.6, once: false });
+
     return (
-        <aside className="flex flex-col w-10/12 backdrop-blur-md bg-white/10 border border-white/20 shadow-xl shadow-black/20 p-6 rounded-2xl gap-5">
+        <motion.div className="flex flex-col w-10/12 backdrop-blur-md bg-white/10 border border-white/20 shadow-xl shadow-black/20 p-6 rounded-2xl gap-5"
+            ref={ref}
+            initial={{ opacity: 0, y: 90, scale: 0.95 }} /* animation propierties inicials */
+            animate={
+                isInView
+                    ? { opacity: 1, y: 0, scale: 1 } /* visble */
+                    : { opacity: 0, y: 35, scale: 0.95 } /* not visible */
+            }
+            transition={{ duration: 0.5, ease: "easeOut" }}>
 
             <div className="flex justify-between items-center">
                 <div className="flex jaustify-start gap-2">
-                    <img className="w-4.5 h-4.5"  src={typescript} alt="" />
-                    <img className="w-5 h-5"  src={react} alt="" />
+                    <img className="w-4.5 h-4.5" src={typescript} alt="" />
+                    <img className="w-5 h-5" src={react} alt="" />
                     <img className="w-8 h-4.5" src={tailwind} alt="" />
                 </div>
 
                 <div className="flex items-center gap-2">
-                    <img  className="w-4.5 h-4.5 text-amber-50" src={github} alt="" />
+                    <img className="w-4.5 h-4.5 text-amber-50" src={github} alt="" />
                     <a href={link}><img className="w-8 h-8 animate-wiggle object-cover" src={www} alt="" /></a>
                 </div>
 
@@ -31,7 +44,7 @@ const ProjectCard: React.FC<Project> = ({ img, tittle, description, link }) => {
                 <p className="text-black font-poppins">{description}</p>
             </div>
 
-        </aside>
+        </motion.div>
     );
 
 }
