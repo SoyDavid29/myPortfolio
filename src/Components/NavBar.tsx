9fb1778805d60732d88f2fe48d6ce94076b046c2
DavidRodriguez <studiandoprogramacion@gmail.com>
2026-03-30 17:41:08 -0600

    style: padding in x modifyed to inheance the design on desktop

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -7,7 +7,7 @@ const NavBar: React.FC = () => {
     <nav className="
       flex items-start fixed top-10 z-50
       left-1/2 -translate-x-1/2 w-auto
-      lg:w-full lg:justify-between lg:left-0 lg:translate-x-0 lg:px-30
+      lg:w-full lg:justify-between lg:left-0 lg:translate-x-0 lg:px-47
     ">
       <HelloBtn />
       <HomeBtn />
